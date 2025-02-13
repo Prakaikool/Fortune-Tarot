271c5bdf151f2315704e2adf7ad367095aa19f6e
Prakaikool <prakaikool.teepraken@gmail.com>
2025-02-13 04:22:59 +0100

    Install and clear all the files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
-vue
+<script setup>
+
+</script>
+
 <template>
   <div>
     <h1>Vi är grymma Richard</h1>
   </div>
 </template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-js
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+import router from './router'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
