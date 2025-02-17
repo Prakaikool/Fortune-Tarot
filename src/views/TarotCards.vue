bc9fd183f2c2ab669e1e00b22a8203efc0e45b95
Prakaikool <prakaikool.teepraken@gmail.com>
2025-02-17 03:39:07 +0100

    Add select option

--- src/views/TarotCards.vue
+++ src/views/TarotCards.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+</script>
 
 <template>
   <div class="tarot-app">
@@ -6,6 +8,20 @@
 
     <!-- Search Input -->
     <input v-model="search" placeholder="Search for a card..." />
+
+    <!-- Filter by type -->
+    <select v-model="SelectedTyp" @change="resetSuitOnMinorSelect">
+      <option value="">All types</option>
+      <option value="major">Major Arcana</option>
+      <option value="minor">Minor Arcana</option>
+    </select>
+
+    <select v-model="selectedSuit" v-if="selectedType === 'minor'"></select>
+    <option value="">All Suits</option>
+    <option value="wands">Wands</option>
+    <option value="swords">Swords</option>
+    <option value="pentacles">Pentacles</option>
+    <option value="cups">Cups</option>
   </div>
 </template>
 
