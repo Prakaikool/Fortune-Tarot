b1417e7c591d496c133cc975dfd1d8347dcc31d0
Prakaikool <prakaikool.teepraken@gmail.com>
2025-02-23 18:35:18 +0100

    Add data

--- src/views/FortuneTarot.vue
+++ src/views/FortuneTarot.vue
@@ -2,14 +2,29 @@
 
 <template>
   <div class="fortune-container">
-    <div>
+    <div class="fortune-welcoming">
       <h1>Fortune Tarot</h1>
     </div>
     <div>
-      Welcome to the world of Fortune Tarot, where the cards hold the keys to your past, present,
-      and future. On this page, you can draw three tarot cards and receive insights that may guide
-      your path. Whether you're seeking answers, clarity, or a touch of magic, the cards are here to
-      reveal your destiny.
+      <p>
+        Welcome to the world of Fortune Tarot, where the cards hold the keys to your past, present,
+        and future. On this page, you can draw three tarot cards and receive insights that may guide
+        your path. Whether you're seeking answers, clarity, or a touch of magic, the cards are here
+        to reveal your destiny.
+      </p>
+    </div>
+
+    <div class="step">
+      <h2>How to play...</h2>
+    </div>
+
+    <div class="fortune-app">
+      <h2>Draw three Tarot cards here!</h2>
+      <p>*Three cards: past, present and future</p>
+
+      <input type="button" @click="drawCards" value="DRAW!">
+
+      
     </div>
   </div>
 </template>
